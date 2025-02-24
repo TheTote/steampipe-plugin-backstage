3c55c63cd3ad3d3036ab91743b6a80581365c13a
Clement HUSSENOT-DESENONGES <clement.hussenot-desenonges@believe.com>
2025-02-24 16:38:24 +0100

    chore: Reorganize the common fields

--- backstage/common.go
+++ backstage/common.go
@@ -7,17 +7,21 @@ import (
 
 // Common columns that appear in most catalog tables
 var commonColumns = []*plugin.Column{
-	{Name: "apiVersion", Type: proto.ColumnType_STRING, Description: "API version of the entity"},
-	{Name: "kind", Type: proto.ColumnType_STRING, Description: "Kind of the entity"},
-	{Name: "name", Type: proto.ColumnType_STRING, Description: "Name of the entity"},
-	{Name: "namespace", Type: proto.ColumnType_STRING, Description: "Namespace of the entity"},
-	{Name: "uid", Type: proto.ColumnType_STRING, Description: "Unique identifier of the entity"},
-	{Name: "title", Type: proto.ColumnType_STRING, Description: "Display title of the entity"},
-	{Name: "description", Type: proto.ColumnType_STRING, Description: "Description of the entity"},
+	// Required metadata fields
+	{Name: "name", Type: proto.ColumnType_STRING, Description: "The name of the entity"},
+	{Name: "namespace", Type: proto.ColumnType_STRING, Description: "The namespace the entity belongs to"},
+	{Name: "kind", Type: proto.ColumnType_STRING, Description: "The kind of the entity"},
+
+	// Common fields
+	{Name: "metadata", Type: proto.ColumnType_JSON, Description: "The full metadata of the entity"},
+	{Name: "spec", Type: proto.ColumnType_JSON, Description: "The specification data of the entity"},
+	{Name: "relations", Type: proto.ColumnType_JSON, Description: "The relations of the entity to other entities"},
+
+	// Optional metadata fields
+	{Name: "title", Type: proto.ColumnType_STRING, Description: "A display name of the entity"},
+	{Name: "description", Type: proto.ColumnType_STRING, Description: "A description of the entity"},
 	{Name: "labels", Type: proto.ColumnType_JSON, Description: "Labels attached to the entity"},
-	{Name: "annotations", Type: proto.ColumnType_JSON, Description: "Annotations on the entity"},
-	{Name: "links", Type: proto.ColumnType_JSON, Description: "Links associated with the entity"},
-	{Name: "tags", Type: proto.ColumnType_JSON, Description: "Tags associated with the entity"},
-	{Name: "metadata", Type: proto.ColumnType_JSON, Description: "Full metadata of the entity"},
-	{Name: "spec", Type: proto.ColumnType_JSON, Description: "Full specification of the entity"},
+	{Name: "annotations", Type: proto.ColumnType_JSON, Description: "Annotations attached to the entity"},
+	{Name: "tags", Type: proto.ColumnType_JSON, Description: "A list of tags attached to the entity"},
+	{Name: "links", Type: proto.ColumnType_JSON, Description: "A list of external hyperlinks related to the entity"},
 }
